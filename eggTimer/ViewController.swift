4c7b4a268fd14e546d459d6a9bbcea4bb45e7aef
West Kraemer <WestKraemer@gmail.com>
2017-01-12 16:40:36 -0500

    begin building timer and timer behavior

--- eggTimer/ViewController.swift
+++ eggTimer/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    
+    func decreaseTimer() {
+        
+    }
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBAction func play(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+    }
+    
+    @IBAction func plusTen(_ sender: Any) {
+    }
+    
+    @IBAction func minusTen(_ sender: Any) {
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
