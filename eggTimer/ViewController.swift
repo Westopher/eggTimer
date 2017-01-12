cfc62e79406e41f2203d09382675e1b963daea05
West Kraemer <WestKraemer@gmail.com>
2017-01-12 17:34:13 -0500

    added additional buttons to control the timer and added comments to explain its workings.

--- eggTimer/ViewController.swift
+++ eggTimer/ViewController.swift
@@ -12,7 +12,23 @@ class ViewController: UIViewController {
 
     var timer = Timer()
     
+    //staring time
+    var time = 210
+    
+    // As long as the Timer is greater than zero, count down by 1. Once the timer hits zero, it stops.
     func decreaseTimer() {
+        
+        if time > 0 {
+        
+            time -= 1
+        
+            timerLabel.text = String(time)
+        
+        } else {
+            
+            timer.invalidate()
+            
+        }
         
     }
     
@@ -25,15 +41,39 @@ class ViewController: UIViewController {
     }
     
     @IBAction func pause(_ sender: Any) {
+        
+        timer.invalidate()
     }
     
+    //If time is greater than 210, the add button won't work.
     @IBAction func plusTen(_ sender: Any) {
+        
+        if time <= 200 {
+        
+        time += 10
+        
+        timerLabel.text = String(time)
+        }
     }
     
+    
+    //If time is greater than 10 seconds, this button will take ten seconds off the count.
     @IBAction func minusTen(_ sender: Any) {
+        
+        if time > 10 {
+        
+            time -= 10
+        
+            timerLabel.text = String(time)
+        }
     }
     
     @IBAction func reset(_ sender: Any) {
+        
+        time = 210
+        
+        timerLabel.text = String(time)
+        
     }
     
     override func viewDidLoad() {
